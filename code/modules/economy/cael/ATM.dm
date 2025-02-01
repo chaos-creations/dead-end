out-of-phaze/Nebula

Remove remaining (excess) machinery Topic overrides

code/modules/economy/cael/ATM.dm
@@ -248,28 +248,29 @@
 	else
 		return
 
-/obj/machinery/atm/Topic(var/href, var/href_list)
+/obj/machinery/atm/OnTopic(mob/user, href_list)
 	if((. = ..()))
 		return
 	if(href_list["choice"])
+		. = TOPIC_REFRESH
 		switch(href_list["choice"])
 			if("transfer")
 				if(authenticated_account)
 					var/transfer_amount = text2num(href_list["funds_amount"])
 					transfer_amount = round(transfer_amount, 0.01)
 					if(transfer_amount <= 0)
-						alert("That is not a valid amount.")
+						alert(user, "That is not a valid amount.")
 					else if(transfer_amount <= authenticated_account.money)
 						var/target_account_number = text2num(href_list["target_acc_number"])
 						var/transfer_purpose = href_list["purpose"]
 						var/datum/money_account/target_account = get_account(target_account_number)
 						if(target_account && authenticated_account.transfer(target_account, transfer_amount, transfer_purpose))
-							to_chat(usr, "[html_icon(src)]<span class='info'>Funds transfer successful.</span>")
+							to_chat(user, "[html_icon(src)]<span class='info'>Funds transfer successful.</span>")
 						else
-							to_chat(usr, "[html_icon(src)]<span class='warning'>Funds transfer failed.</span>")
+							to_chat(user, "[html_icon(src)]<span class='warning'>Funds transfer failed.</span>")
 
 					else
-						to_chat(usr, "[html_icon(src)]<span class='warning'>You don't have enough funds to do that!</span>")
+						to_chat(user, "[html_icon(src)]<span class='warning'>You don't have enough funds to do that!</span>")
 			if("view_screen")
 				view_screen = text2num(href_list["view_screen"])
 			if("change_security_level")
@@ -283,7 +284,7 @@
 				if(held_card)
 					login_card = held_card
 				else
-					login_card = scan_user(usr)
+					login_card = scan_user(user)
 
 				if(!ticks_left_locked_down)
 					var/tried_account_num = text2num(href_list["account_num"])
@@ -317,11 +318,11 @@
 								if(failed_account)
 									failed_account.log_msg("Unauthorized login attempt", machine_id)
 							else
-								to_chat(usr, "[html_icon(src)] <span class='warning'>Incorrect pin/account combination entered, [max_pin_attempts - number_incorrect_tries] attempts remaining.</span>")
+								to_chat(user, "[html_icon(src)] <span class='warning'>Incorrect pin/account combination entered, [max_pin_attempts - number_incorrect_tries] attempts remaining.</span>")
 								previous_account_number = tried_account_num
 								playsound(src, 'sound/machines/buzz-sigh.ogg', 50, 1)
 						else
-							to_chat(usr, "[html_icon(src)] <span class='warning'>Unable to log in to account, additional information may be required.</span>")
+							to_chat(user, "[html_icon(src)] <span class='warning'>Unable to log in to account, additional information may be required.</span>")
 							number_incorrect_tries = 0
 					else
 						playsound(src, 'sound/machines/twobeep.ogg', 50, 1)
@@ -331,7 +332,7 @@
 						//create a transaction log entry
 						authenticated_account.log_msg("Remote terminal access", machine_id)
 
-						to_chat(usr, "[html_icon(src)] <span class='info'>Access granted. Welcome user '[authenticated_account.owner_name].'</span>")
+						to_chat(user, "[html_icon(src)] <span class='info'>Access granted. Welcome user '[authenticated_account.owner_name].'</span>")
 
 					previous_account_number = tried_account_num
 			if("e_withdrawal")
@@ -339,10 +340,10 @@
 				amount = round(amount, 0.01)
 				var/obj/item/charge_stick/E = charge_stick_type
 				if(amount <= 0)
-					alert("That is not a valid amount.")
+					alert(user, "That is not a valid amount.")
 				else if(amount > initial(E.max_worth))
 					var/decl/currency/cur = GET_DECL(initial(E.currency) || global.using_map.default_currency)
-					alert("That amount exceeds the maximum amount holdable by charge sticks from this machine ([cur.format_value(initial(E.max_worth))]).")
+					alert(user, "That amount exceeds the maximum amount holdable by charge sticks from this machine ([cur.format_value(initial(E.max_worth))]).")
 				else if(authenticated_account && amount > 0)
 					//create an entry in the account transaction log
 					if(authenticated_account.withdraw(amount, "Credit withdrawal", machine_id))
@@ -350,23 +351,23 @@
 						E = new charge_stick_type(loc)
 						E.adjust_worth(amount)
 						E.creator = authenticated_account.owner_name
-						usr.put_in_hands(E)
+						user.put_in_hands(E)
 					else
-						to_chat(usr, "[html_icon(src)]<span class='warning'>You don't have enough funds to do that!</span>")
+						to_chat(user, "[html_icon(src)]<span class='warning'>You don't have enough funds to do that!</span>")
 			if("withdrawal")
 				var/amount = max(text2num(href_list["funds_amount"]),0)
 				amount = round(amount, 0.01)
 				if(amount <= 0)
-					alert("That is not a valid amount.")
+					alert(user, "That is not a valid amount.")
 				else if(authenticated_account && amount > 0)
 					//remove the money
 					if(authenticated_account.withdraw(amount, "Credit withdrawal", machine_id))
 						playsound(src, 'sound/machines/chime.ogg', 50, 1)
-						var/obj/item/cash/cash = new(get_turf(usr))
+						var/obj/item/cash/cash = new(get_turf(user))
 						cash.adjust_worth(amount)
-						usr.put_in_hands(src)
+						user.put_in_hands(src)
 					else
-						to_chat(usr, "[html_icon(src)]<span class='warning'>You don't have enough funds to do that!</span>")
+						to_chat(user, "[html_icon(src)]<span class='warning'>You don't have enough funds to do that!</span>")
 			if("balance_statement")
 				if(authenticated_account)
 					var/txt
@@ -424,20 +425,20 @@
 				if(!held_card)
 					//this might happen if the user had the browser window open when somebody emagged it
 					if(emagged > 0)
-						to_chat(usr, "[html_icon(src)] <span class='warning'>The ATM card reader rejected your ID because this machine has been sabotaged!</span>")
+						to_chat(user, "[html_icon(src)] <span class='warning'>The ATM card reader rejected your ID because this machine has been sabotaged!</span>")
 					else
-						var/obj/item/I = usr.get_active_held_item()
+						var/obj/item/I = user.get_active_held_item()
 						if (istype(I, /obj/item/card/id))
-							if(!usr.try_unequip(I, src))
+							if(!user.try_unequip(I, src))
 								return
 							held_card = I
 				else
-					release_held_id(usr)
+					release_held_id(user)
 			if("logout")
 				authenticated_account = null
 				account_security_level = 0
-
-	interact(usr)
+			else
+				. = TOPIC_NOACTION // Unhandled, href hack or just a subtype's command?
 
 /obj/machinery/atm/proc/scan_user(mob/living/human/human_user)
 	if(!authenticated_account)

chaos-creations/dead-end

code/modules/economy/cael/ATM.dm
@@ -248,28 +248,29 @@
 	else
 		return
 
-/obj/machinery/atm/Topic(var/href, var/href_list)
+/obj/machinery/atm/OnTopic(mob/user, href_list)
 	if((. = ..()))
 		return
 	if(href_list["choice"])
+		. = TOPIC_REFRESH
 		switch(href_list["choice"])
 			if("transfer")
 				if(authenticated_account)
 					var/transfer_amount = text2num(href_list["funds_amount"])
 					transfer_amount = round(transfer_amount, 0.01)
 					if(transfer_amount <= 0)
-						alert("That is not a valid amount.")
+						alert(user, "That is not a valid amount.")
 					else if(transfer_amount <= authenticated_account.money)
 						var/target_account_number = text2num(href_list["target_acc_number"])
 						var/transfer_purpose = href_list["purpose"]
 						var/datum/money_account/target_account = get_account(target_account_number)
 						if(target_account && authenticated_account.transfer(target_account, transfer_amount, transfer_purpose))
-							to_chat(usr, "[html_icon(src)]<span class='info'>Funds transfer successful.</span>")
+							to_chat(user, "[html_icon(src)]<span class='info'>Funds transfer successful.</span>")
 						else
-							to_chat(usr, "[html_icon(src)]<span class='warning'>Funds transfer failed.</span>")
+							to_chat(user, "[html_icon(src)]<span class='warning'>Funds transfer failed.</span>")
 
 					else
-						to_chat(usr, "[html_icon(src)]<span class='warning'>You don't have enough funds to do that!</span>")
+						to_chat(user, "[html_icon(src)]<span class='warning'>You don't have enough funds to do that!</span>")
 			if("view_screen")
 				view_screen = text2num(href_list["view_screen"])
 			if("change_security_level")
@@ -283,7 +284,7 @@
 				if(held_card)
 					login_card = held_card
 				else
-					login_card = scan_user(usr)
+					login_card = scan_user(user)
 
 				if(!ticks_left_locked_down)
 					var/tried_account_num = text2num(href_list["account_num"])
@@ -317,11 +318,11 @@
 								if(failed_account)
 									failed_account.log_msg("Unauthorized login attempt", machine_id)
 							else
-								to_chat(usr, "[html_icon(src)] <span class='warning'>Incorrect pin/account combination entered, [max_pin_attempts - number_incorrect_tries] attempts remaining.</span>")
+								to_chat(user, "[html_icon(src)] <span class='warning'>Incorrect pin/account combination entered, [max_pin_attempts - number_incorrect_tries] attempts remaining.</span>")
 								previous_account_number = tried_account_num
 								playsound(src, 'sound/machines/buzz-sigh.ogg', 50, 1)
 						else
-							to_chat(usr, "[html_icon(src)] <span class='warning'>Unable to log in to account, additional information may be required.</span>")
+							to_chat(user, "[html_icon(src)] <span class='warning'>Unable to log in to account, additional information may be required.</span>")
 							number_incorrect_tries = 0
 					else
 						playsound(src, 'sound/machines/twobeep.ogg', 50, 1)
@@ -331,7 +332,7 @@
 						//create a transaction log entry
 						authenticated_account.log_msg("Remote terminal access", machine_id)
 
-						to_chat(usr, "[html_icon(src)] <span class='info'>Access granted. Welcome user '[authenticated_account.owner_name].'</span>")
+						to_chat(user, "[html_icon(src)] <span class='info'>Access granted. Welcome user '[authenticated_account.owner_name].'</span>")
 
 					previous_account_number = tried_account_num
 			if("e_withdrawal")
@@ -339,10 +340,10 @@
 				amount = round(amount, 0.01)
 				var/obj/item/charge_stick/E = charge_stick_type
 				if(amount <= 0)
-					alert("That is not a valid amount.")
+					alert(user, "That is not a valid amount.")
 				else if(amount > initial(E.max_worth))
 					var/decl/currency/cur = GET_DECL(initial(E.currency) || global.using_map.default_currency)
-					alert("That amount exceeds the maximum amount holdable by charge sticks from this machine ([cur.format_value(initial(E.max_worth))]).")
+					alert(user, "That amount exceeds the maximum amount holdable by charge sticks from this machine ([cur.format_value(initial(E.max_worth))]).")
 				else if(authenticated_account && amount > 0)
 					//create an entry in the account transaction log
 					if(authenticated_account.withdraw(amount, "Credit withdrawal", machine_id))
@@ -350,27 +351,26 @@
 						E = new charge_stick_type(loc)
 						E.adjust_worth(amount)
 						E.creator = authenticated_account.owner_name
-						usr.put_in_hands(E)
+						user.put_in_hands(E)
 					else
-						to_chat(usr, "[html_icon(src)]<span class='warning'>You don't have enough funds to do that!</span>")
+						to_chat(user, "[html_icon(src)]<span class='warning'>You don't have enough funds to do that!</span>")
 			if("withdrawal")
 				var/amount = max(text2num(href_list["funds_amount"]),0)
 				amount = round(amount, 0.01)
 				if(amount <= 0)
-					alert("That is not a valid amount.")
+					alert(user, "That is not a valid amount.")
 				else if(authenticated_account && amount > 0)
 					//remove the money
-					// TODO: Jesus Christ why does this entire proc use usr
 					if(authenticated_account.withdraw(amount, "Credit withdrawal", machine_id))
 						playsound(src, 'sound/machines/chime.ogg', 50, 1)
-						var/cash_turf = get_turf(usr)
+						var/cash_turf = get_turf(user)
 						var/obj/item/cash/cash = new(cash_turf, null, amount)
 						if(QDELETED(cash))
 							cash = locate() in cash_turf
 						if(cash)
-							usr.put_in_hands(cash)
+							user.put_in_hands(cash)
 					else
-						to_chat(usr, "[html_icon(src)]<span class='warning'>You don't have enough funds to do that!</span>")
+						to_chat(user, "[html_icon(src)]<span class='warning'>You don't have enough funds to do that!</span>")
 			if("balance_statement")
 				if(authenticated_account)
 					var/txt
@@ -428,20 +428,20 @@
 				if(!held_card)
 					//this might happen if the user had the browser window open when somebody emagged it
 					if(emagged > 0)
-						to_chat(usr, "[html_icon(src)] <span class='warning'>The ATM card reader rejected your ID because this machine has been sabotaged!</span>")
+						to_chat(user, "[html_icon(src)] <span class='warning'>The ATM card reader rejected your ID because this machine has been sabotaged!</span>")
 					else
-						var/obj/item/I = usr.get_active_held_item()
+						var/obj/item/I = user.get_active_held_item()
 						if (istype(I, /obj/item/card/id))
-							if(!usr.try_unequip(I, src))
+							if(!user.try_unequip(I, src))
 								return
 							held_card = I
 				else
-					release_held_id(usr)
+					release_held_id(user)
 			if("logout")
 				authenticated_account = null
 				account_security_level = 0
-
-	interact(usr)
+			else
+				. = TOPIC_NOACTION // Unhandled, href hack or just a subtype's command?
 
 /obj/machinery/atm/proc/scan_user(mob/living/human/human_user)
 	if(!authenticated_account)